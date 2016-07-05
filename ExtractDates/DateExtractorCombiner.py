f12f54754cd46456aaf8c35a52e201d9a3d55b8a
ashwintk <tashwin@okstate.edu>
2016-07-05 02:26:17 -0500

    Changed Date Extraction Code

--- ExtractDates/DateExtractorCombiner.py
+++ ExtractDates/DateExtractorCombiner.py
@@ -1,33 +1,16 @@
 #!/usr/bin/env python
 import sys
 
-current_word = None
-current_count = 0
-word = None
+dateArray =[]
 
 for line in sys.stdin:
     # remove leading and trailing whitespace
-    line = line.strip()
+    line = line.strip().lstrip()
     # parse the input we got from mapper.py
-    word, count = line.split(',', 1)
-    # convert count (currently a string) to int
-    try:
-        count = int(count)
-    except ValueError:
-        # count was not a number, so silently
-        # ignore/discard this line
-        continue
-    # this IF-switch only works because Hadoop sorts map output
-    # by key (here: word) before it is passed to the reducer
-    if current_word == word:
-        current_count += count
-    else:
-        if current_word:
-            # write result to STDOUT
-            print '%s\t%s' % (current_word, current_count)
-        current_count = count
-        current_word = word
+    tempArray = line.split(',')
+    if len(tempArray) == 2:
+        if tempArray[0] not in dateArray:
+            dateArray.append(tempArray[0])
 
-# do not forget to output the last word if needed!
-if current_word == word:
-    print '%s\t%s' % (current_word, current_count)
+for item in dateArray:
+    print "%s,1" %(item)
