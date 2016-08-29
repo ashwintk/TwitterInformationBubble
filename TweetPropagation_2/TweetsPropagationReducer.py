7eb884fca80abf489e170acf118c8cc7fa6941e2
Ashwin Kumar <tashwin@okstate.edu>
2016-08-29 03:25:48 -0500

    Completed Second part of Tweet Propagation

--- TweetPropagation_2/TweetsPropagationReducer.py
+++ TweetPropagation_2/TweetsPropagationReducer.py
@@ -1,103 +1,123 @@
 #!/usr/bin/env python
-
-import json, sys, re, os
-
+import sys, os
+from itertools import chain, imap
+from dateutil import parser
+from datetime import timedelta as td
 sys.path.append('./')
-
 import TweetsLib as tlib
 
-owner_dict = {}
-propagation_dict = {}
+# This class is used to represent the users who tweet as tree data structure
+class twitter_user:
+    # Init method to initialize a twitter user node
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+    # Iterate method to iterate over the entire tree
+    def __iter__(self):
+        for v in chain(*imap(iter, self.children)):
+            yield v
+        yield self.value
+    # This method adds a child to the tree
+    def addChild(self,value):
+        self.children.append(value)
+# This method iterates through the entire tree recursively and adds a child node to appropriate node
+# Arguments:
+            #1. Root Node of the tree
+            #2. Node in tree for which a child node must be created
+            #3. Value of the child node
+# This function doesn't return anything
+def addChildIfOwnerExistsInTree(twitter_obj, value, childToAppend):
+    if twitter_obj.value.strip().lstrip() == value.strip().lstrip():
+        twitter_obj.addChild(twitter_user(childToAppend))
+    elif len(twitter_obj.children) == 0 and twitter_obj.value != value:
+        return
+    else:
+        for child in twitter_obj.children:
+            addChildIfOwnerExistsInTree(child, value, childToAppend)
 
-fileName = '/Users/ashwinkumar/Desktop/ProcessedRetweet.txt'
-
-with open(fileName,"r") as readHandle:
-    for line in readHandle.readlines():
-        strArray = line.split("\t")
-        if len(strArray) == 3:
-            currentUser = strArray[1]
-            immideateOwner = strArray[2]
-            strArray = None
-            if immideateOwner.lstrip().strip() == "N/A,N/A":
-                owner_dict.update({currentUser: "1"})
-            else:
-                tempArray = currentUser.split(",")
-                timestamp = tlib.convertStringToTimestamp(tempArray[0])
-                if propagation_dict.has_key(timestamp):
-                    prop_arr = propagation_dict[timestamp]
-                    prop_arr.append(currentUser+"\t"+immideateOwner)
-                    propagation_dict[timestamp] = prop_arr
-                else:
-                    prop_arr = []
-                    prop_arr.append(currentUser + "\t" + immideateOwner)
-                    propagation_dict.update({timestamp:prop_arr})
-readHandle.close()
-
-for key in owner_dict.keys():
-    print key
-
-for key in sorted(propagation_dict):
-    print propagation_dict[key]
-    
-    
-    ''' My Code '''
-    class Tweet:
-    ''' Input expected in the order of <TEXT>\t<TIMESTAMP>\t
-    <USER HANDLE>\t<OWNER Handle>\t<ORIGINAL TIMESTAMP>'''
-    def __init__(self, *args): '''takes the input '''
-	    self.text = args[0]
-        self.timestamp = args[1]
-        self.user = args[2]
-        self.owner = args[3]
-        self.own_timestamp = args[4]
-        self.level = 0
-
-    def is_retweet(self):
-        if self.owner == "N/A":
-            return False;
+# This method iterates through the entire tree recursively to find if a node is present in a tree
+# Arguments:
+            #1. Root Node of the tree
+            #2. Node in tree fwhose existance must be checked
+# This function returns true if the node of interest is found
+def checkIfANodeExistsInTree(twitter_obj, value):
+    if twitter_obj.value.strip().lstrip() == value.strip().lstrip():
         return True
-    def print_tweet(self):
-        print self.text, self.user, self.timestamp, self.owner, self.own_timestamp
-tweet_list= []
+    elif len(twitter_obj.children) == 0 and twitter_obj.value != value:
+        return None
+    else:
+        for child in twitter_obj.children:
+            checkIfANodeExistsInTree(child, value)
+# Dictionary to temporarily store the levels of a tweet
+myLevels = {}
+# This method extracts levles of a tweet from a tree recursively
+# Arguments:
+            # 1. Root Node of the tree
+            # 2. Level count (Initially it will be zero, since it is a recursive method, it will be incremented as desired)
+# This function returns nothing, but populates the dictionary declared above
+def extractLevelsFromATree(twitter_obj, level_count):
+    if len(twitter_obj.children) >= 0:
+        (ts,_) = twitter_obj.value.strip().lstrip().split(",")
+        if ts != "root":
+            thisDate = tlib.extractDateFromTimestamp(ts)
+            if not myLevels.has_key(str(thisDate)):
+                myLevels.update({str(thisDate): str(level_count)})
+            else:
+                dict_val = myLevels[str(thisDate)]
+                dict_val += "," + str(level_count)
+                myLevels.update({str(thisDate): dict_val})
+        for child in twitter_obj.children:
+            extractLevelsFromATree(child, level_count+1)
+    else:
+        return
+# Initialize the tree with a dummy node
+owner = twitter_user("root,root")
+# Get the start and end date passed from command line
+startDate = parser.parse(os.environ["START_DATE"])
+endDate = parser.parse(os.environ["END_DATE"])
+# This date list will contain the dates from the start to the end date
+date_list = [str((startDate + td(days=x)).date()) for x in range(0, (endDate-startDate).days)]
+# Initialize the last tweet to none
+last_tweet = None
 
-def read_input(inputFile): ''' reads input file '''
-    with open(inputFile,"r") as input:
-        for line in input:
-            line = line.split("\t")
-            tweet = Tweet(*line)
-            tweet_list.append(tweet)   
-        tweet_list.sort(key= lambda Tweet: Tweet.timestamp, reverse=False) ''' inbuilt sort function '''
-def tweet_propagation():
-    dic = {}
-    length = len(tweet_list)
-    for i in range(0, length):
-        if tweet_list[i].is_retweet():
-            continue
-        temp_list = [tweet_list[i]]
-        for j in range(i, length):
-            if tweet_list[j].is_retweet() and (tweet_list[i].text == tweet_list[j].text) and (tweet_list[i].user == tweet_list[j].owner):
-                for tweet in temp_list:
-                    if tweet.user == tweet_list[j].owner:
-                        tweet_list[j].level = tweet.level +1 '''incrementing the previous level after the match is made '''
-                        break
-                temp_list.append(tweet_list[j])
-        if tweet_list[i].user in dic:
-            dic[tweet_list[i].user][tweet_list[i].text] = temp_list[1:]
-        else:
-            dic[tweet_list[i].user]={}
-            dic[tweet_list[i].user][tweet_list[i].text] =temp_list[1:]
-    return dic
-read_input("output.txt") ''' input file named output.txt is read '''
-dic=tweet_propagation()
-count=0
-for user in dic :
-    for tweet in dic[user]:
-        li = dic[user][tweet]
-        for tweetObj in li:
-            print tweetObj.timestamp
-            print tweetObj.level
-            count +=1
-        print
-    print
+# for every tweet read
+for line in sys.stdin:
+    # Split the line to tweet, current owner infor and previous owner info.
+    (this_tweet, this_owner, prev_owner) = line.strip().lstrip().split("\t")
+    # You should change the code in mapper to avoid the following two lines
+    prev_owner_arr = prev_owner.split(",")
+    prev_owner = prev_owner_arr[1]+","+prev_owner_arr[0]
+    # There is no grouping operation in hadoop streaming, so check if previous tweet and current tweet are the same
+    if last_tweet != this_tweet:
+        if last_tweet is not None:
+            # If the last encountered tweet and the current tweet are different, we will have to process the propagation
+            extractLevelsFromATree(owner, 0)
+            owner = twitter_user("root,root")
+            this_propagation = ['0'] * (endDate - startDate).days
+            for key in myLevels.keys():
+                if key in date_list:
+                    this_propagation[date_list.index(key)] = myLevels[key]
+            myLevels = {}
+            print last_tweet + "\t" + "\t".join(this_propagation)
+            this_propagation = []
+        last_tweet = this_tweet
+    # If the last encountered tweet and the current tweet are the same, append it to the array
+    if prev_owner == "N/A,N/A":
+        owner.addChild(twitter_user(this_owner))
+    else:
+        if checkIfANodeExistsInTree(owner, prev_owner) is not True:
+            owner.addChild(twitter_user(prev_owner))
+        addChildIfOwnerExistsInTree(owner, prev_owner, this_owner)
 
-
+if last_tweet == this_tweet:
+    # If the last encountered tweet and the current tweet are different, we will have to process the propagation
+    extractLevelsFromATree(owner, 0)
+    owner = twitter_user("root,root")
+    this_propagation = ['0'] * (endDate - startDate).days
+    for key in myLevels.keys():
+        if key in date_list:
+            this_propagation[date_list.index(key)] = myLevels[key]
+    myLevels = {}
+    print this_tweet + "\t" + "\t".join(this_propagation)
+    this_propagation = []
+    last_tweet = this_tweet
